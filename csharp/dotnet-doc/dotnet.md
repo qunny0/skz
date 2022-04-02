659f28ae46a2c8fb662ccb272c3b380a8a3b6399
Ben <bei.fu@biubiux2.com>
2022-04-02 18:04:03 +0800

    包引用 。

--- csharp/dotnet-doc/dotnet.md
+++ csharp/dotnet-doc/dotnet.md
@@ -8,3 +8,8 @@ https://docs.microsoft.com/zh-cn/dotnet/core/tutorials/top-level-templates
 ### .NET 实现
 .NET的实现：.NET Framework, .NET 5, .NET Core, Mono.
 https://docs.microsoft.com/zh-cn/dotnet/fundamentals/implementations
+
+
+- 安装dotnet-sdk首先关闭 vs code。 否则定位不到dotnet sdk
+
+- 参数前的 this 什么意思
---
+++ csharp/solution/ClassLibraryProjects/ShowCase/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UtilityLibraries;
+using Newtonsoft.Json;
+
+public class Account
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime DOB { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Account account = new Account
+        {
+            Name = "Ben",
+            Email = "ben@163.com",
+            DOB = new DateTime(1980, 2, 20, 0, 0, 0, DateTimeKind.Utc)
+        };   
+
+        string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+        Console.WriteLine(json);
+
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+                ResetConsole();
+
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input} {"Begins with uppercase? ",30}: " +
+                              $"{(input.StartsWithUpper() ? "Yes" : "No")}{Environment.NewLine}");
+            row += 3;
+        } while (true);
+        return;
+
+        // Declare a ResetConsole local method
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
